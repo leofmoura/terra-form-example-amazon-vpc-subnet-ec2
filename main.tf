a357c35d3a776346b32e63cc913374d85cc3196c
Leo Moura <leofmoura@gmail.com>
2022-08-02 22:24:55 -0400

    Adding instance with provisioner ssh setup

--- main.tf
+++ main.tf
@@ -76,3 +76,40 @@ resource "aws_security_group" "mtc_sg" {
     env  = "dev"
   }
 }
+
+resource "aws_key_pair" "mtc_auth" {
+  key_name = "mtckey"
+  public_key = file("~/.ssh/mtckey.pub")
+
+  tags = {
+    Name = "dev-auth_key"
+    env  = "dev"
+  }
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+  ami = data.aws_ami.server_ami.id
+  key_name = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id = aws_subnet.mtc_public_subnet.id
+  user_data = file("userdata.tpl")
+  
+  root_block_device {
+    volume_size = 9
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("windows-ssh-config.tpl", {
+      hostname = self.public_ip,
+      user = "ubuntu",
+      identityfile = "~/.ssh/mtckey"
+    })
+    interpreter = ["powershell", "-Command"]
+  }
+
+  tags = {
+    Name = "dev-node"
+    env  = "dev"
+  }
+}
